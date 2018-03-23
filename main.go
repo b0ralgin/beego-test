19ceb2c37a894a9525b1f394607d2416a677c720
Kirill Borodulin <kirill.borodylin@gmail.com>
2018-03-23 14:58:58 +0500

    inital commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "github.com/b0ralgin/test-beego/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
