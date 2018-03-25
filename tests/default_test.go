5c7b31794300420d801d3848d9ccd60e396be20f
Kirill Borodulin <kirill.borodylin@gmail.com>
2018-03-25 21:58:00 +0500

    add config for tests

--- tests/default_test.go
+++ tests/default_test.go
@@ -1,38 +1,56 @@
 package test
 
 import (
+	"bytes"
+	"encoding/json"
+	"log"
 	"net/http"
 	"net/http/httptest"
-	"testing"
-	"runtime"
 	"path/filepath"
-	_ "github.com/b0ralgin/test-beego/routers"
+	"runtime"
+	"testing"
 
 	"github.com/astaxie/beego"
+	"github.com/b0ralgin/test-beego/models"
+	_ "github.com/b0ralgin/test-beego/routers"
+	"github.com/b0ralgin/test-beego/utilities"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func init() {
 	_, file, _, _ := runtime.Caller(1)
-	apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, ".." + string(filepath.Separator))))
+	apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, ".."+string(filepath.Separator))))
 	beego.TestBeegoInit(apppath)
+	err := utilities.MongoStartup()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
 
 // TestGet is a sample to run an endpoint test
-func TestGet(t *testing.T) {
-	r, _ := http.NewRequest("GET", "/v1/object", nil)
+
+func TestSignUp(t *testing.T) {
+	user := models.User{
+		Username: "test",
+		Password: "test",
+	}
+	mUser, _ := json.Marshal(user)
+	reqReader := bytes.NewReader(mUser)
+	r, _ := http.NewRequest("POST", "/v1/signup", reqReader)
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
-
 	beego.Trace("testing", "TestGet", "Code[%d]\n%s", w.Code, w.Body.String())
-
-	Convey("Subject: Test Station Endpoint\n", t, func() {
-	        Convey("Status Code Should Be 200", func() {
-	                So(w.Code, ShouldEqual, 200)
-	        })
-	        Convey("The Result Should Not Be Empty", func() {
-	                So(w.Body.Len(), ShouldBeGreaterThan, 0)
-	        })
+	response := models.User{}
+	json.Unmarshal(w.Body.Bytes(), &response)
+	Convey("Subject: SingUp Endpoint\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+		Convey("The Result Should Not Be Empty", func() {
+			So(w.Body.Len(), ShouldBeGreaterThan, 0)
+		})
+		Convey("The result Should Be Equal", func() {
+			So(response.Username, ShouldEqual, user.Username)
+		})
 	})
 }
-
